d3ef3cd3e08c350320d9d4f58d221fbb45fa54c1
Nathan Walker <earthworn@gmail.com>
2013-04-08 10:43:16 -0500

    Added XCode Project and OCDSpec2.

---
+++ fTTT/fTTT/FranklinAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FranklinAppDelegate.h
+//  fTTT
+//
+//  Created by Nathan Walker on 4/8/13.
+//  Copyright (c) 2013 Nathan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FranklinAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fTTT/fTTT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fTTT
+//
+//  Created by Nathan Walker on 4/8/13.
+//  Copyright (c) 2013 Nathan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FranklinAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([FranklinAppDelegate class]));
+  }
+}
