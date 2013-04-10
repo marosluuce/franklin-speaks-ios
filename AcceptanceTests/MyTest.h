e3351bfc2a02bf4c968d37d89ae53f9506aca4ba
Nathan Walker <earthworn@gmail.com>
2013-04-09 22:09:10 -0500

    Try something.

---
+++ AcceptanceTests/MyTest.h
@@ -0,0 +1,13 @@
+//
+//  MyTest.h
+//  fTTT
+//
+//  Created by Nathan Walker on 4/9/13.
+//  Copyright (c) 2013 Nathan Walker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyTest : NSObject
+
+@end
